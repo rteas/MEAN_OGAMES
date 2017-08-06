dc375cc03359a3908b607902a22e1d7b4443628c
Richy <richyteas@gmail.com>
2017-08-06 03:29:01 +0000

    maybe changeddir1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/dist";
 app.use(express.static(distDir));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
