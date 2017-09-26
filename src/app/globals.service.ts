039aa5427c67d40961c7033f3203065b958daad8
Richy <richyteas@gmail.com>
2017-09-26 22:26:04 +0000

    improving login aesthetics

--- src/app/globals.service.ts
+++ src/app/globals.service.ts
@@ -3,11 +3,13 @@ import { User } from './users/user';
 import { Room } from './rooms/room';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Game, AUTO } from 'phaser-ce';
 
 @Injectable()
 export class GlobalService {
     
   userInfo: User;
   roomInfo: Room;
+  gameInfo: Game;
   
 }
