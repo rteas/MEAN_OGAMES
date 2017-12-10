0542ed09d89d15378d3cd525a78e6780ed1c1b65
Richy <richyteas@gmail.com>
2017-12-10 23:44:48 +0000

    making user-details page more generic, location data as room name

--- src/app/users/user-details/user-details.component.ts
+++ src/app/users/user-details/user-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../../globals.service';
 import { UserService } from '../user.service';
+import { RoomService } from '../../rooms/room.service';
 import { User } from '../user';
+import { Room } from '../../rooms/room';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
@@ -11,10 +14,36 @@ import { User } from '../user';
 export class UserDetailsComponent implements OnInit {
 
   constructor(public globalService: GlobalService,
-              public userService: UserService) { }
+              public userService: UserService,
+              private roomService: RoomService,
+              private activatedRoute: ActivatedRoute) { }
+  user: User;
   userFriends : User[] = [];
+  room: Room;
   
   ngOnInit() {
+    // TODO: get user by id
+    let id = this.activatedRoute.snapshot.paramMap.get('id').toString();
+    
+    this.userService.getUser(id)
+                    .then(user => {
+                      if (!user.friends){
+                        user.friends = [];
+                      }
+                      if (!user.location) {
+                        user.location = 'unknown';
+                      }
+                      // if user is in a room, get the room data
+                      else{
+                        this.roomService.getRoom(user.location)
+                                        .then( room => {
+                                          this.room = room;
+                                        });
+                      }
+                      this.user = user;
+                    });
+    
+    // populate friends
     var friends = this.globalService.userInfo.friends;
     for(var i = 0; i < friends.length; i++){
       this.userService.getUser(friends[i])
@@ -23,6 +52,14 @@ export class UserDetailsComponent implements OnInit {
                           this.userFriends.push(tempUser);
                         });
     }
+    
+    
+    
   }
+  
+  populateRoom(){
+    
+  }
+  
 
 }
